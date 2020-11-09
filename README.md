512ed5bbc43f517c7d4c426b0cd6c6f2e8e0ca39
jyhere <jyhere@gmail.com>
2020-11-09 16:18:22 +0100

    - CHANGES : better output, code formatting, variable names - ADDED : command arguments

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Retropie2powkiddy
 
-A tool for exporting a RetroPie system to a Powkiddy understandable XML.
+A RetroPie system export tool for Powkiddy A12 retro arcade machine.
+
+This python script will copy roms, convert art images and generate the gamelist XML with full game names for the Powkiddy A12. 
+
+Although it has been tested on Powkiddy A12 only, it may works with other Powkiddy consoles like the new A13.
--- retropie2powkiddy.py
+++ retropie2powkiddy.py
@@ -8,10 +8,7 @@
 #
 # Author : Jyhere
 #
-# Thanks to Wagner TechTalk for the genGamelist.py script
-#
-# This Software is provided AS-IS and without
-# Warranty, etc. Use at your own risk.
+# Based on Wagner TechTalk genGamelist.py script
 #
 
 import sys, getopt, shutil, os, pathlib, os.path
@@ -19,6 +16,33 @@ from os import path
 from PIL import Image
 from lxml import etree
 from xml.sax.saxutils import escape
+
+retroPieTargetSystem = powKiddyTargetSystem = ''
+
+
+def main(argv):
+    global retroPieTargetSystem, powKiddyTargetSystem
+    try:
+        opts, args = getopt.getopt(argv, "hr:p:", ["retropiesystem=", "powkiddysystem="])
+    except getopt.GetoptError:
+        print('retropie2powkiddy.py -r <retropiesystem> -p <powkiddysystem>')
+        print('Supported <powkiddysystem> values : CPS, FBA, FC, GB')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -r <retropiesystem> -p <powkiddysystem>')
+            sys.exit()
+        elif opt in ("-r", "--retropiesystem"):
+            retroPieTargetSystem = arg
+        elif opt in ("-p", "--powkiddysystem"):
+            powKiddyTargetSystem = arg
+    print('RetroPie system is "' + retroPieTargetSystem)
+    print('powkiddy system is "' + powKiddyTargetSystem)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
 
 # ====== IMPORTANT =======
 # Root path of retropie installation
@@ -57,32 +81,32 @@ retroPieGameListXml = retroPieRootPath + '/configs/all/emulationstation/gamelist
 retroPieGameListImagesPath = retroPieRootPath + '/configs/all/emulationstation/downloaded_images/' + retroPieTargetSystem
 retroPieGameListRomsPath = retroPieRomsPath + '/' + retroPieTargetSystem
 
-if (not path.exists(retroPieGameListXml)):
+if not path.exists(retroPieGameListXml):
     print("File " + retroPieGameListXml + 'is missing')
     exit()
-if (not path.exists(retroPieGameListRomsPath)):
+if not path.exists(retroPieGameListRomsPath):
     print("Directory " + retroPieGameListXml + 'is missing')
     exit()
-if (not path.exists(retroPieGameListImagesPath)):
+if not path.exists(retroPieGameListImagesPath):
     print("Directory " + retroPieGameListXml + 'is missing')
     exit()
 
 gameList = {}
-if (path.exists(retroPieGameListXml)):
+if path.exists(retroPieGameListXml):
     tree = etree.parse(retroPieGameListXml)
 
     for game in tree.xpath("/gameList/game"):
         gameProperties = list(game.iter())
         hasPath = hasName = False
         for gameProperty in gameProperties:
-            if (gameProperty.tag == 'path'):
+            if gameProperty.tag == 'path':
                 tmpPath = gameProperty.text
                 hasPath = True
-            if (gameProperty.tag == 'name'):
+            if gameProperty.tag == 'name':
                 gameName = gameProperty.text
                 hasName = True
 
-        if (hasPath and hasName):
+        if hasPath and hasName:
             archiveName = tmpPath.lstrip('./')
             iFileExtensionIndex = tmpPath.rindex('.')
             gameFileExtension = tmpPath[iFileExtensionIndex:]
@@ -117,27 +141,28 @@ try:
 except FileExistsError:
     print('Game Directory ', exportGameSubdir, ' already exists.')
 
+print('Exporting ' + retroPieTargetSystem + 'games:')
+
 iPageCount = round(gameListCount / pageSize)
 xmlOutput = ''
 xmlOutput = xmlOutput + '<?xml version="1.0"?>   ' + CRLF
 xmlOutput = xmlOutput + '<strings_resources>   ' + CRLF
 xmlOutput = xmlOutput + '<icon_para game_list_total=' + str(gameListCount) + '></icon_para>   ' + CRLF
 
-iCurrentPageCount = 1
-# Item (Game)
-iItemCount = 1
-# Item (game number) on the current Page
-iItemOnPageCount = 0
+currentPage = currentItem = 1
+currentItemOnPage = 0
+
 # Indicates if page closing is needed
-bPageOpen = False
-sGameName = ''
+pageOpened = False
 
 for gameShortName, gameData in gameList.items():
+
+    print(gameShortName + '... ', end='')
+
     # Page header
-    if (iItemOnPageCount == 0):
-        #print('Page: ' + str(iCurrentPageCount))
-        xmlOutput = xmlOutput + '  <icon_page' + str(iCurrentPageCount) + '> ' + CRLF
-        bPageOpen = True
+    if currentItemOnPage == 0:
+        xmlOutput = xmlOutput + '  <icon_page' + str(currentPage) + '> ' + CRLF
+        pageOpened = True
 
     # Copying roms
     sourceFilePathName = retroPieGameListRomsPath + '/' + gameData['file']
@@ -145,45 +170,42 @@ for gameShortName, gameData in gameList.items():
     shutil.copyfile(sourceFilePathName, newFilePathName)
 
     # Copy Image file with the new name
-    if (createArtImages):
+    if createArtImages:
         sourceImageFile = retroPieGameListImagesPath + '/' + gameShortName + '-image'
-        # Only supporting .jpg and .png files at this time.  .jpg files
-        # will be converted to .png to work on the A12
+        # convert art if format is JPG
         sourceImageFileExistsJPG = path.exists(sourceImageFile + '.jpg')
         sourceImageFileExistsPNG = path.exists(sourceImageFile + '.png')
-        if (sourceImageFileExistsJPG):
+        if sourceImageFileExistsJPG:
             # Convert .jpg artwork to .png (needed for Powkiddy A12)
-            sDestImageFile = sDestinationImagePath + gameShortName + '.png'
-            #print(' Destination Path: ' + sDestImageFile)
+            destImageFile = sDestinationImagePath + gameShortName + '.png'
             img = Image.open(sourceImageFile + '.jpg')
-            img.save(sDestImageFile)
-            #shutil.copyfile( sSourceImageFile, sDestImageFile)
-
-        elif (sourceImageFileExistsPNG):
-             sDestImageFile = sDestinationImagePath + '/'  + sGameName + '.png'
-             shutil.copyfile(sourceImageFile + '.png', sDestImageFile)
-        elif (createNoArtImage):
+            img.save(destImageFile)
+        elif sourceImageFileExistsPNG:
+            destImageFile = sDestinationImagePath + '/' + gameShortName + '.png'
+            shutil.copyfile(sourceImageFile + '.png', destImageFile)
+        elif createNoArtImage:
             # If unable to copy, use default image (you can customize this, if you want)
             sourceImageFile = noArtFilename
-            sDestImageFile = sDestinationImagePath + '/'  + sGameName + '.png'
-            shutil.copyfile(sourceImageFile, sDestImageFile)
+            destImageFile = sDestinationImagePath + '/' + gameShortName + '.png'
+            shutil.copyfile(sourceImageFile, destImageFile)
 
-        xmlOutput = xmlOutput + '    <icon' + str(iItemOnPageCount) + '_para  name=\"' + str(iItemCount) + '.' + escape(gameData['name']) + '\" game_path=\"' + gameData['file'] + '\"></icon' + str(iItemOnPageCount) + '_para> ' + CRLF
+        xmlOutput = xmlOutput + '    <icon' + str(currentItemOnPage) + '_para  name=\"' + str(currentItem) + '.' + escape(gameData['name']) + '\" game_path=\"' + gameData['file'] + '\"></icon' + str(currentItemOnPage) + '_para> ' + CRLF
 
+    print('OK')
     # Page Suffix
-    if (iItemOnPageCount == pageSize -1 or (iItemCount == gameListCount and iItemOnPageCount + 1 == pageSize - 1)):
-        xmlOutput = xmlOutput + '  </icon_page' + str(iCurrentPageCount) + '> ' + CRLF
-        iItemOnPageCount = -1
-        iCurrentPageCount += 1
-        bPageOpen = False
+    if (currentItemOnPage == pageSize - 1) or (currentItem == gameListCount and currentItemOnPage + 1 == pageSize - 1):
+        xmlOutput = xmlOutput + '  </icon_page' + str(currentPage) + '> ' + CRLF
+        currentItemOnPage = -1
+        currentPage += 1
+        pageOpened = False
 
-    if (iItemCount <=gameListCount):
-        iItemCount += 1
-        iItemOnPageCount += 1
+    if currentItem <= gameListCount:
+        currentItem += 1
+        currentItemOnPage += 1
 
 # Page Suffix (if not already added)
-if (bPageOpen):
-    xmlOutput = xmlOutput + '  </icon_page' + str(iCurrentPageCount) + '> ' + CRLF
+if pageOpened:
+    xmlOutput = xmlOutput + '  </icon_page' + str(currentPage) + '> ' + CRLF
 
 # File end tag
 xmlOutput = xmlOutput + '</strings_resources>   ' + CRLF
@@ -200,6 +222,6 @@ oFile.close()
 # Output a summary
 print('*** SUMMARY OF ACTIONS ***')
 print('\"' + retroPieTargetSystem + '/' + outputFileName + '\" created.')
-print('Games found and Processed : ' + str(iItemCount-1) )
-print('Pages generated : ' + str(iCurrentPageCount) + ' [up to ' + str(pageSize) + ' games per page]')
+print('Games found and Processed : ' + str(currentItem - 1))
+print('Pages generated : ' + str(currentPage) + ' [up to ' + str(pageSize) + ' games per page]')
 
